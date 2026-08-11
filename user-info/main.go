06e44639da76100067e1e345d94eab5425e991e0
pehunter <patrickhunter1212@gmail.com>
2026-08-11 15:29:26 +0000

    add endpoints to retrieve patterns/progress

--- user-info/main.go
+++ user-info/main.go
@@ -48,14 +48,99 @@ func register(db *pgx.Conn) gin.HandlerFunc {
 		}
 
 		//Everything good; insert the user
-		_, err := db.Query(context.Background(), "insert into crochet_user (name, password) values ($1, $2)", newUser.Name, newUser.Password)
+		row, err := db.Query(context.Background(), "insert into crochet_user (name, password) values ($1, $2) returning id", newUser.Name, newUser.Password)
 		if err != nil {
 			fmt.Printf("%s", err.Error())
 			ctx.JSON(http.StatusInternalServerError, jsonError("An error occurred trying to insert the user"))
 			return
 		}
 
-		ctx.Status(200)
+		//Attempt to retrieve id
+		id, err := pgx.CollectExactlyOneRow(row, func(row pgx.CollectableRow) (int32, error) {
+			var i int32
+			err := row.Scan(&i)
+			return i, err
+		})
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s", err.Error())
+			ctx.JSON(http.StatusInternalServerError, jsonError("An internal serfver error occurred"))
+			return
+		}
+
+		ctx.JSON(200, gin.H{"id": id})
+	}
+}
+
+// Get all patterns IDs created by a user
+func created_patterns(db *pgx.Conn) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		//Check ID param
+		uid, ok := c.Params.Get("id")
+		if !ok {
+			c.JSON(http.StatusBadRequest, jsonError("No User ID was given"))
+			return
+		}
+
+		//Attempt to fetch rows
+		rows, err := db.Query(context.Background(), "select id from crochet_pattern where creator_id = $1", uid)
+		if err != nil {
+			fmt.Printf("%s", err.Error())
+			c.JSON(http.StatusInternalServerError, jsonError("An internal server error occurred"))
+			return
+		}
+
+		//Collect rows
+		patterns, err := pgx.CollectRows(rows, func(row pgx.CollectableRow) (int32, error) {
+			var id int32
+			err := row.Scan(&id)
+			return id, err
+		})
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s", err.Error())
+			c.JSON(http.StatusInternalServerError, jsonError("An internal server error occurred"))
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"patterns": patterns})
+	}
+}
+
+// Get all patterns IDs created by a user
+func created_progress(db *pgx.Conn) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		//Check ID param
+		uid, ok := c.Params.Get("id")
+		if !ok {
+			c.JSON(http.StatusBadRequest, jsonError("No User ID was given"))
+			return
+		}
+
+		//Attempt to fetch rows
+		rows, err := db.Query(context.Background(), "select id from crochet_progress where user_id = $1", uid)
+		if err != nil {
+			fmt.Printf("%s", err.Error())
+			c.JSON(http.StatusInternalServerError, jsonError("An internal server error occurred"))
+			return
+		}
+
+		//Collect rows
+		progress, err := pgx.CollectRows(rows, func(row pgx.CollectableRow) (int32, error) {
+			var id int32
+			err := row.Scan(&id)
+			return id, err
+		})
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s", err.Error())
+			c.JSON(http.StatusInternalServerError, jsonError("An internal server error occurred"))
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"progress": progress})
 	}
 }
 
@@ -70,6 +155,8 @@ func main() {
 
 	router := gin.Default()
 	router.POST("/register", register(conn))
+	router.GET("/patterns/:id", created_patterns(conn))
+	router.GET("/progress/:id", created_progress(conn))
 
 	router.Run("localhost:8080")
 }
